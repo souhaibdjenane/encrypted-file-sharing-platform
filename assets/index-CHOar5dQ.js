e4319448fda6db2357165c19c31a366794b0eae0
souhaibdjenane <souhaibdjenane@gmail.com>
2026-02-27 23:11:28 +0100

    Updates

--- assets/index-CHOar5dQ.js
+++ assets/index-CHOar5dQ.js
@@ -68,7 +68,7 @@ ${z}`}class Xe extends Error{constructor({message:n,code:s,cause:r,name:o}){var 
           disabled:opacity-50 disabled:cursor-not-allowed
           ${n?"border-vs-danger":"border-vs-border"}
           ${s}
-        `,...o}),n&&w.jsx("p",{className:"text-xs text-vs-danger",children:n})]})}function cv({mode:i="signin",className:n="",...s}){const{t:r}=rn();async function o(){await Xt.auth.signInWithOAuth({provider:"google",options:{redirectTo:`${window.location.origin}/dashboard`,queryParams:{access_type:"offline",prompt:"consent"}}})}return w.jsxs("button",{type:"button",onClick:o,className:`
+        `,...o}),n&&w.jsx("p",{className:"text-xs text-vs-danger",children:n})]})}function cv({mode:i="signin",className:n="",...s}){const{t:r}=rn();async function o(){await Xt.auth.signInWithOAuth({provider:"google",options:{redirectTo:`${window.location.origin}/encrypted-file-sharing-platform/dashboard`,queryParams:{access_type:"offline",prompt:"consent"}}})}return w.jsxs("button",{type:"button",onClick:o,className:`
         w-full flex items-center justify-center gap-3 px-4 py-2.5 rounded-lg text-sm font-medium
         bg-white dark:bg-zinc-800 text-slate-700 dark:text-zinc-200
         border border-slate-300 dark:border-zinc-600
