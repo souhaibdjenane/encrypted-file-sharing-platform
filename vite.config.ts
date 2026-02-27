9cc0f8a7c939e8c50a040868602dc576a84c9262
souhaibdjenane <souhaibdjenane@gmail.com>
2026-02-27 22:46:46 +0100

    chore: configuring github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/encrypted-file-sharing-platform/',   // 👈 change this to your repo name
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
