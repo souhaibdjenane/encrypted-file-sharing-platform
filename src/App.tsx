816a5064e9fd5d42be09f6f21accaa96834590fe
souhaibdjenane <souhaibdjenane@gmail.com>
2026-02-27 22:56:36 +0100

    fix: Fix landpage 404 error

--- src/App.tsx
+++ src/App.tsx
@@ -40,7 +40,7 @@ function AppRoutes() {
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <AppRoutes />
     </BrowserRouter>
   )
